63e9b225b19a1a859d400950609c0304140e9fe2
Eric Munoz <128002939+Ericolink@users.noreply.github.com>
2026-03-12 15:58:18 -0600

    Se corrigio bug

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola, esta app se desplego con CD en Railway 🚀"
+    return "Pipeline CD funcionando en Railway 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
-    
